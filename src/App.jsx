b1af60bb7a755dcee429ffe7cad739f1750a9494
mlyb25 <mlybianto@gmail.com>
2025-03-03 20:20:16 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import CurrencyDropdown from "./CurrencyDropdown";
+import CurrencyContext from "./contexts/CurrencyContext";
 
 function App() {
+  const [currencyFrom, setCurrencyFrom] = useState("USD");
+  const [currencyTo, setCurrencyTo] = useState("SGD");
 
   return (
     <>
       <h1>Currency Converter</h1>
-      <button>
-        Click me
-      </button>
+      <CurrencyContext.Provider
+        value={{
+          currencyFrom,
+          setCurrencyFrom,
+          currencyTo,
+          setCurrencyTo,
+        }}
+      >
+        <CurrencyDropdown />
+      </CurrencyContext.Provider>
     </>
-  )
+  );
 }
 
 export default App;
